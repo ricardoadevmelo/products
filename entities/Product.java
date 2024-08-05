4b5dc100ab64c7076ad75f97f93ea12533aa6d07
Ricardo Melo <ricardoadevmelo@gmail.com>
2024-08-05 08:59:38 +0100

    get and set added

--- entities/Product.java
+++ entities/Product.java
@@ -5,6 +5,9 @@ public class Product {
     public double price;
     public int quantity;
 
+    public Product(){
+    }
+
     public Product(String name, double price, int quantity){
         this.name = name;
         this.price = price;
@@ -13,6 +16,23 @@ public class Product {
     public Product(String name, double price){
         this.name = name;
         this.price = price;
+    }
+
+
+    public String getName(String name){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public double getPrice(double price){
+        return price;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+    public int getQuantity(int quantity){
+        return quantity;
     }
 
     public double totalValueInStock (){
